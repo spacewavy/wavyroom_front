a195ccda5310a8f4373c76f5e9a2ea0dc35b545e
conan-kim <conan.kim@hyperhire.in>
2023-11-17 18:00:19 +0900

    Modified: detail page

--- app/model-detail/page.tsx
+++ app/model-detail/page.tsx
@@ -7,7 +7,15 @@ import Image from "next/image";
 import ModelDetailCarousel from "../../components/ModelDetailCarousel";
 
 const ModelDetail = () => {
+  const COLORS = [
+    { hex: "#ffffff", name: "흰색" },
+    { hex: "#DADAD9", name: "연한회색" },
+    { hex: "#D7D5CC", name: "회색" },
+    { hex: "#3D4F36", name: "초록" },
+    { hex: "#4B4842", name: "먹색" },
+  ];
   const [isDark, setIsDark] = useState(false);
+  const [selectedColor, setSelectedColor] = useState(COLORS[0]);
 
   return (
     <main className="flex flex-col flex-1">
@@ -71,12 +79,42 @@ const ModelDetail = () => {
         <div className="flex flex-col items-center py-40 bg-lightGray">
           <Image src={ImageNova} alt="nova" />
         </div>
-        <div className="flex flex-col items-center pt-8">
-          <div>색깔 고르기 영역</div>
+        <div className="flex flex-col items-center pt-8 pb-20 lg:pb-25">
+          <div className="flex flex-col items-center gap-4">
+            <div className="flex flex-row items-center gap-2">
+              {COLORS.map((item, index) => {
+                const isSelected = selectedColor.hex === item.hex;
+                return (
+                  <div
+                    key={"color" + index}
+                    className="relative w-8 h-8 p-1 cursor-pointer"
+                    onClick={() => {
+                      setSelectedColor(item);
+                    }}
+                  >
+                    <div
+                      className="w-full h-full rounded-full"
+                      style={{
+                        backgroundColor: item.hex,
+                        borderWidth: 1,
+                        borderColor: "rgba(0, 0, 0, 0.1)",
+                      }}
+                    />
+                    {isSelected && (
+                      <div className="absolute bg-black top-0 bottom-0 left-0 right-0 bg-transparent border-[2px] border-black rounded-full" />
+                    )}
+                  </div>
+                );
+              })}
+            </div>
+            <div className="font-normal text-[14px]">{selectedColor.name}</div>
+          </div>
         </div>
       </section>
       <section className="bg-gray">
-        <div>Images</div>
+        <div className="flex flex-col items-center py-[125px] px-6">
+          <Image src={ImageNova} alt="nova" />
+        </div>
       </section>
       <section className="bg-lightGray">
         <div>Detail info</div>
