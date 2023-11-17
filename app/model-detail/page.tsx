fb4ba1f3741815db52146814728846ff8e3321ac
conan-kim <conan.kim@hyperhire.in>
2023-11-17 18:56:04 +0900

    modified: detail #2

--- app/model-detail/page.tsx
+++ app/model-detail/page.tsx
@@ -5,6 +5,7 @@ import ImageNova from "@/assets/Products/Nova.svg";
 import Button from "../../components/Button";
 import Image from "next/image";
 import ModelDetailCarousel from "../../components/ModelDetailCarousel";
+import FaqItem from "../../components/FaqItem";
 
 const ModelDetail = () => {
   const COLORS = [
@@ -14,8 +15,77 @@ const ModelDetail = () => {
     { hex: "#3D4F36", name: "초록" },
     { hex: "#4B4842", name: "먹색" },
   ];
+
+  const FAQs = [
+    {
+      question: "Wavyroom은 다른 모듈식 하우스와 어떻게 다르나요?",
+      answer:
+        "무통장 입금은 상품 구매 대금은 PC뱅킹, 인터넷뱅킹, 텔레뱅킹 혹은 가까운 은행에서 직접 입금하시면 됩니다.주문시 입력한 입금자명과 실제입금자의 성명이 반드시 일치하여야 하며, 7일 이내로 입금을 하셔야 하며 입금되지 않은 주문은 자동취소 됩니다.",
+    },
+    {
+      question: "장치가 완성된 형태로 운송되나요?",
+      answer:
+        "무통장 입금은 상품 구매 대금은 PC뱅킹, 인터넷뱅킹, 텔레뱅킹 혹은 가까운 은행에서 직접 입금하시면 됩니다.주문시 입력한 입금자명과 실제입금자의 성명이 반드시 일치하여야 하며, 7일 이내로 입금을 하셔야 하며 입금되지 않은 주문은 자동취소 됩니다.",
+    },
+    {
+      question: "Wavyroom은 배송 시간은 어떻게 되나요?",
+      answer:
+        "무통장 입금은 상품 구매 대금은 PC뱅킹, 인터넷뱅킹, 텔레뱅킹 혹은 가까운 은행에서 직접 입금하시면 됩니다.주문시 입력한 입금자명과 실제입금자의 성명이 반드시 일치하여야 하며, 7일 이내로 입금을 하셔야 하며 입금되지 않은 주문은 자동취소 됩니다.",
+    },
+    {
+      question: "Wavyroom은 몇년 동안 사용하기에 적합하나요?",
+      answer:
+        "무통장 입금은 상품 구매 대금은 PC뱅킹, 인터넷뱅킹, 텔레뱅킹 혹은 가까운 은행에서 직접 입금하시면 됩니다.주문시 입력한 입금자명과 실제입금자의 성명이 반드시 일치하여야 하며, 7일 이내로 입금을 하셔야 하며 입금되지 않은 주문은 자동취소 됩니다.",
+    },
+    {
+      question: "Wavyroom은 다른 모듈식 하우스와 어떻게 다르나요?",
+      answer:
+        "무통장 입금은 상품 구매 대금은 PC뱅킹, 인터넷뱅킹, 텔레뱅킹 혹은 가까운 은행에서 직접 입금하시면 됩니다.주문시 입력한 입금자명과 실제입금자의 성명이 반드시 일치하여야 하며, 7일 이내로 입금을 하셔야 하며 입금되지 않은 주문은 자동취소 됩니다.",
+    },
+  ];
+
+  const DETAIL_INFO = [
+    {
+      title: "가격",
+      description: "￦72,000,000~",
+    },
+    {
+      title: "디폴트 크기",
+      description: "8 평형 (28㎡) (3.3m W x 8m D Height: 3.3m)",
+    },
+    {
+      title: "외부 색",
+      description: "Silver",
+    },
+    {
+      title: "지붕 색",
+      description: "TBD",
+    },
+    {
+      title: "스트럭쳐",
+      description: "CFS(Cold Formed Steel) 냉간성형강구조",
+    },
+    {
+      title: "마감",
+      description: "Prin Anodizing Aluminum (프린 아노다이징 알루미늄)",
+    },
+    {
+      title: "용도",
+      description: "호텔 객실을 모듈화 한 것. 하이엔드 리조트, 펜션으로 적합",
+    },
+  ];
+
   const [isDark, setIsDark] = useState(false);
   const [selectedColor, setSelectedColor] = useState(COLORS[0]);
+
+  const InfoDetail = ({ detail }: any) => {
+    return (
+      <div className="flex flex-col border-b border-midGray py-4 gap-2">
+        <div className="text-[12px] opacity-40">{detail.title}</div>
+        <div className="text-[12px]">{detail.description}</div>
+      </div>
+    );
+  };
 
   return (
     <main className="flex flex-col flex-1">
@@ -111,16 +181,44 @@ const ModelDetail = () => {
           </div>
         </div>
       </section>
-      <section className="bg-gray">
-        <div className="flex flex-col items-center py-[125px] px-6">
-          <Image src={ImageNova} alt="nova" />
+      <section>
+        <div className="flex flex-col lg:flex-row">
+          <div className="flex flex-col items-center justify-center py-[125px] px-6 bg-gray lg:bg-lightGray flex-1">
+            <Image src={ImageNova} alt="nova" />
+          </div>
+          <div className="flex flex-col bg-white lg:bg-lightGray flex-1 px-8 py-20">
+            <div className="text-[28px] pb-4 border-b border-midGray">
+              Evo 타입 상세정보
+            </div>
+            {DETAIL_INFO.map((item, index) => {
+              return <InfoDetail key={"detail" + index} detail={item} />;
+            })}
+            <div className="py-4 text-[12px]">
+              - 웨이비룸을 처음으로 제품느낌나도록 메탈릭한 exterior finish를
+              사용
+              <br />- 주방x- 침실, 욕실, 옷장, 금고, 등 호텔객실의 레이아웃을
+              거의 그대로 가져옴
+              <br />- 내부 마감재가 다양 (벽: white + wood, 바닥: white tile +
+              카펫)
+            </div>
+          </div>
         </div>
       </section>
-      <section className="bg-lightGray">
-        <div>Detail info</div>
-      </section>
-      <section>
-        <div>FAQ</div>
+      <section className="bg-lightGray"></section>
+      <section className="px-6 md:px-8 lg:px-[280px] py-20 lg:py-25">
+        <div className="flex flex-col items-center gap-16 w-full">
+          <div className="text-[28px] md:text-[32px]">자주 묻는 질문</div>
+          <div className="flex flex-col w-full">
+            {FAQs.map((item, index) => (
+              <FaqItem
+                key={"faq" + index}
+                question={item.question}
+                answer={item.answer}
+              />
+            ))}
+          </div>
+          <Button name="카카오톡 상담하기" arrow varient="lightGray" />
+        </div>
       </section>
       <section>
         <div>List of models</div>
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,36 +1,37 @@
-import { Slot } from '@radix-ui/react-slot';
-import { cva, type VariantProps } from 'class-variance-authority';
-import * as React from 'react';
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
+import * as React from "react";
 
-import RightArrowOrange from '@/assets/icons/RightArrowOrange.svg';
-import { cn } from '@/lib/utils';
-import Image from 'next/image';
+import RightArrowOrange from "@/assets/icons/RightArrowOrange.svg";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center rounded-md text-xs font-normal h-[34px] ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+  "inline-flex items-center justify-center rounded-md text-xs font-normal h-[34px] ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
-        default: 'bg-black text-white',
-        outline: 'bg-white text-black border border-black',
-        secondary: 'bg-gray text-black',
-        ghost: 'bg-white text-black',
-        ghostOrange: 'bg-white text-orange',
+        default: "bg-black text-white",
+        outline: "bg-white text-black border border-black",
+        secondary: "bg-gray text-black",
+        ghost: "bg-white text-black",
+        ghostOrange: "bg-white text-orange",
+        lightGray: "bg-lightGray text-black",
         destructive:
-          'bg-destructive text-destructive-foreground hover:bg-destructive/90',
-        link: 'text-black underline-offset-4 hover:underline',
-        underline: 'text-black underline-offset-4 underline',
+          "bg-destructive text-destructive-foreground hover:bg-destructive/90",
+        link: "text-black underline-offset-4 hover:underline",
+        underline: "text-black underline-offset-4 underline",
       },
       size: {
-        default: 'px-4 py-2 rounded-3xl text-xs',
-        sm: 'rounded-md px-3 text-xs',
-        lg: 'rounded-md px-8 text-xl',
-        icon: 'h-10 w-10',
+        default: "px-4 py-2 rounded-3xl text-xs",
+        sm: "rounded-md px-3 text-xs",
+        lg: "rounded-md px-8 text-xl",
+        icon: "h-10 w-10",
       },
     },
     defaultVariants: {
-      variant: 'default',
-      size: 'default',
+      variant: "default",
+      size: "default",
     },
   }
 );
@@ -43,7 +44,7 @@ export interface ButtonProps
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : 'button';
+    const Comp = asChild ? Slot : "button";
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
@@ -53,7 +54,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     );
   }
 );
-Button.displayName = 'Button';
+Button.displayName = "Button";
 
 interface CustomButtomProps extends ButtonProps {
   withArrow?: boolean;
@@ -66,9 +67,9 @@ const GhostOrangeButton = ({
   name: string;
   withIcon?: boolean;
 }) => (
-  <Button variant='ghostOrange'>
+  <Button variant="ghostOrange">
     {name}
-    {withIcon && <Image src={RightArrowOrange} alt='ghost-orange' />}
+    {withIcon && <Image src={RightArrowOrange} alt="ghost-orange" />}
   </Button>
 );
 
