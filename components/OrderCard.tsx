b5da2dbe21baabb62fa2f7661c8d9bf5bbc1a0f4
touqeershakeel <touqeershakeel@gmail.com>
2023-11-22 12:49:34 +0500

    Added tailwind css instead of css

--- components/OrderCard.tsx
+++ components/OrderCard.tsx
@@ -6,37 +6,34 @@ export interface OrderCardProps {
   text: string;
   image:any;
   imageRight:boolean;
-  isLast:boolean;
 }
 
-const OrderCard = ({ heading, subheading, text, image, imageRight,isLast=false }: OrderCardProps) => {
+const OrderCard = ({ heading, subheading, text, image, imageRight }: OrderCardProps) => {
   return (
-    <div className="grid w-full grid-cols-1 md:grid-cols-3 px-4 sm:px-8 md:px-12 lg:px-50 xl:px-60 gap-4 md:gap-0" style={{direction: imageRight ? "rtl" : "ltr", minHeight: !isLast ? '384px' :''}}>
+    <div className="grid w-full grid-cols-1 md:grid-cols-3 gap-4 md:gap-0 max-h-fit md:min-h-[262px] lg:min-h-[379px] last:min-h-fit" style={{direction: imageRight ? "rtl" : "ltr"}}>
       <div className="imageSection" >
-        <Image src={image} alt="order_image" className="flex items-center justify-center w-full h-" />
+        <Image src={image} alt="order_image" className="flex items-center justify-center w-full"/>
       </div>
       
-      <div style={{alignItems:'center', flexDirection:'column'}} className="hidden md:flex">
+      <div className="hidden md:flex items-center md:flex-col">
         <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 15 15" fill="none">
           <circle cx="7.5" cy="7.5" r="7" fill="white" stroke="#B2B2B2"/>
         </svg>
-        <div style={{height:'100%', width:"1px",backgroundColor:'#B2B2B2'}}></div>
+        <div className="h-full w-[1px] bg-midGray"></div>
       </div>
 
-      <div className="contentSection">
-        <div className="flex justify-between">
-          <div className="text-black text-bodyMD md:text-bodyLG">
-            <h1>
-              <span style={{fontSize:"32px", fontWeight:300, color:"#000000", lineHeight:"41.6px", gap:"24px"}}>{heading} </span>
-            </h1>
-            <p>
-            <span style={{fontSize:"20px", fontWeight:300, lineHeight:"26px", color:"#000000", gap:"8px"}}>{subheading}</span>
-            </p>
+      <div className="contentSection text-left px-4 md:px-0">
+          <div>
+            <div className="mb-4 md:mb-[24px]">
+              <span className="text-[24px] md:text-[32px] font-light">{heading} </span>
+            </div>
+            <div className="mb-2">
+              <span className="text-[14px] md:text-[20px] font-light">{subheading}</span>
+            </div>
             <div>
-            <span style={{fontSize:"14px", fontWeight:300, lineHeight:"22.4px", color:"#6E6E73"}}>{text}</span>
+              <span className="text-[12px] md:text-[14px] font-light text-wavyGary">{text}</span>
             </div>
           </div>
-        </div>
       </div>
     </div>
   );
