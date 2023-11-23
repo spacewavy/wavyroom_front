cddb433b53841e87886ff57c24865e5a76d72d95
touqeer <touqeershakeel@gmail.com>
2023-11-24 01:33:32 +0500

    Fixed some styling

--- app/customization/page.tsx
+++ app/customization/page.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import Image from "next/image";
 import CustomImg from "@/assets/Customization/image.png";
 import LeftArrow from "@/assets/icons/LeftArrowIcons.svg";
@@ -8,14 +10,21 @@ import CustomItems from "@/components/customization/CustomItems";
 import CustomizationPanel from "@/components/customization/CustomizationPanel";
 
 const Customization = () => {
+  const [navigateToSettings, setNavigateToSettings] = useState(false);
+
+  const moveToCustomSettings = (value: boolean) => {
+    console.log(value)
+    setNavigateToSettings(value)
+  }
+
 return(
-  <div className="flex flex-col lg:flex-row justify-center">
-      <div className="w-full lg:w-[65.5%] bg-[#F9F9FA]">
+  <div className="flex flex-col lg:flex-row justify-center max-w-[100vw] overflow-hidden h-[100vh]">
+      <div className="w-full lg:w-[65%] bg-[#F9F9FA] flex flex-col">
           <div className="flex pt-8 pl-8 pb-[24px] gap-[8px]">
             <Image src={LeftArrow} alt='leftarrow' />
             <Image className="mx-2 my-2" src={Vector} alt='vector'/>
           </div>
-          <div className="pt-[229px] pb-[220px] flex justify-center">
+          <div className="flex-1 flex justify-center">
            <Image src={CustomImg} alt="img"/> 
           </div>
           <div className="flex flex-col items-center pb-8 gap-[20px] lg:text-[14px] md:text-sm">
@@ -23,10 +32,17 @@ return(
             <p>모델을 마우스로 드래그하여 구성을 회전하세요 </p>
           </div>
         </div>
-      <div className="lg:w-[34.4%] md:w-full max-h-[100vh]  relative">
-        <CustomItems/>
-        {/* <CustomizationPanel /> */}
+      <div className="lg:w-[35%] md:w-full max-h-[100vh] relative overflow-hidden">
+        <div className={`absolute top-0 left-0 right-0 bottom-0 top-0 transition-transform duration-500 ease-out ${navigateToSettings ? 'translate-x-[-100%]' : 'translate-x-0'}`}>
+          <div className="absolute top-0 left-0  w-full">
+            <CustomItems navigateToSettings={moveToCustomSettings}/>
+          </div>
+          <div className="absolute top-0 left-[100%] w-full">
+            <CustomizationPanel  />
+          </div>
         </div>
+
+      </div>
   </div>
 )
 };
--- components/customization/CustomizationCard.tsx
+++ components/customization/CustomizationCard.tsx
@@ -10,16 +10,16 @@ export interface CustomCardProps{
 
 const CustomizationCard=({heading, subheading, price, image}: CustomCardProps)=>{
   return(
-  <div>
-    <div className="flex justify-between p-[24px] mx-4 hover:bg-[#F9F9FA] rounded-lg">
     <div>
-      <h1 className="text-base font-light color-[#000] mb-[12px]">{heading}</h1>
-      <h2 className="text-0.75rem font-weight mb-[8px] color-[#000]">{subheading}</h2>
-      <p className="text-0.75rem font-weight color-[#000]">{price}</p>
+      <div className="flex justify-between p-[24px] mx-4 hover:bg-[#F9F9FA] rounded-lg">
+        <div>
+          <h1 className="text-base font-light color-[#000] mb-[12px]">{heading}</h1>
+          <h2 className="text-0.75rem font-weight mb-[8px] color-[#000]">{subheading}</h2>
+          <p className="text-0.75rem font-weight color-[#000]">{price}</p>
+        </div>
+        <div className="flex justify-center pt-4 w-[208px]"><Image src={image} alt="img"/></div>
+      </div>
     </div>
-    <div className="flex justify-center pt-4 w-[208px]"><Image src={image} alt="img"/></div>
-    </div>
-  </div>
   )
 };
 export default CustomizationCard;
