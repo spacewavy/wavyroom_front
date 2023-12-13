9ffbd52e88243661658fdde58b579a2ae421c29d
touqeer <touqeershakeel@gmail.com>
2023-12-13 14:08:39 +0500

    Changed the backend api url

--- app/redux/actions/portfolioActions.ts
+++ app/redux/actions/portfolioActions.ts
@@ -7,7 +7,7 @@ export const fetchPortfolioData = (option: string) => {
   return async (dispatch: any) => {
     try {
       const response = await axios.get(
-        `http://13.210.86.154:3000/api/v1/portfolio?size=${option}`
+        `https://test-spacewavy.com/api/v1/portfolio?size=${option}`
       );
       dispatch({
         type: FETCH_PORTFOLIO_DATA_SUCCESS,
