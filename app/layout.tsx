8c5b45a4c919dbff2289721ff14dec7fd11eea67
touqeershakeel <touqeershakeel@gmail.com>
2024-01-03 16:33:31 +0500

    Added font family for english language

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,8 @@
-import { cn } from "@/lib/utils";
-import type { Metadata } from "next";
-import localFont from "next/font/local";
+"use client";
 import "./globals.css";
 import { LayoutProvider } from "./LayoutProvider";
-
-const pretendard = localFont({
-  src: [
-    {
-      path: "../public/fonts/Pretendard-Light.woff2",
-      weight: "300",
-    },
-    {
-      path: "../public/fonts/Pretendard-Medium.woff2",
-      weight: "400",
-    },
-  ],
-  variable: "--font-pretendard",
-});
-
-export const metadata: Metadata = {
-  title: "Spacewavy",
-  description: "",
-};
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 export default function RootLayout({
   children,
@@ -30,8 +11,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={cn(pretendard.className, "flex flex-col font-light")}>
-        <LayoutProvider>{children}</LayoutProvider>
+      <body>
+        <Provider store={store}>
+          <LayoutProvider>{children}</LayoutProvider>
+        </Provider>
       </body>
     </html>
   );
